63b9513b5e3a1de4f35928828fe7502e8bb123c2
john-cheval <johncheval1@gmail.com>
2025-05-09 09:05:44 +0400

    navbar has been finished

---
+++ src/app/lib/navLinks.js
@@ -0,0 +1,32 @@
+export const navLinks = [
+  {
+    id: 1,
+    title: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    title: "About Us",
+    link: "/about",
+  },
+  {
+    id: 3,
+    title: "services",
+    link: "/services",
+  },
+  {
+    id: 4,
+    title: "career",
+    link: "/careers",
+  },
+  {
+    id: 5,
+    title: "events",
+    link: "/events",
+  },
+  {
+    id: 5,
+    title: "contact us",
+    link: "/contact-us",
+  },
+];
