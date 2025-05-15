35f83ef608b512880e18397507de96d10f497468
john-cheval <johncheval1@gmail.com>
2025-05-15 16:54:16 +0400

    events page chages are done

--- src/components/Events/Section1/index.jsx
+++ src/components/Events/Section1/index.jsx
@@ -1,65 +1,24 @@
 "use client";
-import React, { useRef, useState } from "react";
+import React, { useLayoutEffect, useRef, useState } from "react";
 import {
   IoPlayCircleSharp,
   IoLocationOutline,
   IoCalendarClearOutline,
   IoPauseCircle,
 } from "react-icons/io5";
-import { motion } from "framer-motion";
 import isVideo from "@/app/lib/checkVideo";
 import Image from "next/image";
 import useMediaQuery from "@/app/hooks/useMediaQuery";
-
-const imageVariants = {
-  initial: {
-    scale: 1,
-    transition: { duration: 0.5, ease: "easeInOut" },
-  },
-  hover: { scale: 0.8, transition: { duration: 0.5, ease: "easeOut" } },
-};
-
-const playButtonVariants = {
-  initial: { opacity: 0, y: 30 },
-  hover: {
-    opacity: 1,
-    y: "-50%",
-    transition: { duration: 0.5, ease: "easeOut", delay: 0.1 },
-  },
-};
-
-const textVariants = {
-  initial: {
-    opacity: 0,
-    y: 40,
-    transition: { duration: 0.5, ease: "easeOut" },
-  },
-  hover: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.3, ease: "easeOut", delay: 0.1 },
-  },
-};
-
-const textVariants2 = {
-  initial: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.5, ease: "easeOut" },
-  },
-};
-
-const playButtonVariants2 = {
-  initial: {
-    opacity: 1,
-    y: 0,
-    transition: { duration: 0.5, ease: "easeOut" },
-  },
-};
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
 
 const Section1 = () => {
   const [active, setActive] = useState("past");
   const videoRef = useRef(null);
+  const textRef = useRef(null);
+  const playButtonRef = useRef(null);
+
   const isMobile = useMediaQuery("(max-width: 768px)");
 
   const handlePlay = () => {
@@ -81,6 +40,74 @@ const Section1 = () => {
     }
     setIsPlaying(!isPlaying);
   };
+
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      const video = videoRef.current;
+      const button = playButtonRef.current;
+      const text = textRef.current;
+
+      if (!video || !text) return;
+
+      if (isMobile) {
+        gsap.set(video, { scale: 1 });
+        gsap.set(button, { y: 0, opacity: 1 });
+        gsap.set(text, { y: 0, opacity: 1 });
+        return;
+      }
+
+      gsap.fromTo(
+        video,
+        { scale: 1 },
+        {
+          scale: 0.8,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: video,
+            start: "top 30%",
+            end: "bottom center",
+            scrub: true,
+          },
+        }
+      );
+
+      gsap.fromTo(
+        button,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: video,
+            start: "top top+=100",
+            end: "bottom center",
+            scrub: true,
+          },
+        }
+      );
+
+      gsap.fromTo(
+        text,
+        { y: 30, opacity: 0, duration: 300 },
+        {
+          y: 0,
+          opacity: 1,
+          ease: "power2.out",
+          scrollTrigger: {
+            trigger: video,
+            start: "top 20%",
+            end: "bottom center",
+            scrub: true,
+            // markers: true,
+          },
+        }
+      );
+    });
+
+    return () => ctx.revert();
+  }, [isMobile]);
+
   return (
     <section className="  relative ">
       <div className="flex justify-center relative pt-6 md:pt-10 lg:pt-16   ">
@@ -106,28 +133,22 @@ const Section1 = () => {
       </div>
 
       <div className="eventsbg">
-        <h1 className="text-[40px] md:text-[50px] lg:text-[70px] xl:text-[80px] uppercase leading-[118.423%]  text-center py-6 md:py-8">
-          PAST EVENTS
+        <h1 className="text-4xl md:text-[50px]  lg:text-[5vw] xl:text-[80px] uppercase leading-[118.423%]  text-center py-6 md:py-8">
+          {active} EVENTS
         </h1>
 
         <div className="grid grid-cols-12 px-6 md:px-0  ">
-          <motion.div
-            initial="initial"
-            whileHover={isMobile ? undefined : "hover"}
-            animate="initial"
-            className=" col-span-12 md:col-start-3 md:col-end-11 relative overflow-hidden"
-          >
-            <motion.div
-              className="aspect-[16/9] relative mb-6 md:mb-0"
-              variants={isMobile ? {} : imageVariants}
+          <div className=" col-span-12 md:col-start-3 md:col-end-11 relative overflow-hidden">
+            <div
+              className="aspect-[16/9] relative mb-6 md:mb-0 will-change-transform"
+              ref={videoRef}
             >
               {isVideo("/events/hero.jpg") ? (
                 <video
                   loop
-                  ref={videoRef}
                   muted
                   playsInline
-                  autoPlay={false}
+                  autoPlay={isPlaying}
                   poster="/events/hero.jpg"
                   className="object-cover w-full h-full"
                 />
@@ -143,10 +164,7 @@ const Section1 = () => {
                 />
               )}
               {isVideo("/events/hero.jpg") && (
-                <motion.div
-                  className="absolute top-[35%] left-[45%] md:top-1/2 md:left-1/2 -translate-x-1/2 -translate-y-1/2"
-                  variants={isMobile ? playButtonVariants2 : playButtonVariants}
-                >
+                <div className="absolute top-[35%] left-[45%] md:top-1/2 md:left-1/2 -translate-x-1/2 -translate-y-1/2">
                   {isPlaying ? (
                     <IoPauseCircle
                       className=" cursor-pointer"
@@ -157,18 +175,24 @@ const Section1 = () => {
                   ) : (
                     <IoPlayCircleSharp
                       className=" cursor-pointer"
+                      ref={playButtonRef}
+                      style={{ transform: "translateY(50px)", opacity: 0 }}
                       color="#EAE4D6"
                       size={isMobile ? 50 : 100}
                       onClick={handleVideoClick}
                     />
                   )}
-                </motion.div>
+                </div>
               )}
-            </motion.div>
+            </div>
 
-            <motion.div
+            <div
+              ref={textRef}
               className="space-y-1 md:-mt-6"
-              variants={isMobile ? textVariants2 : textVariants}
+              style={{
+                transform: "translateY(40px)",
+                opacity: isMobile ? 1 : 0,
+              }}
             >
               <h3 className="text-center text-3xl lg:text-[40px] font-medium leading-[141%]">
                 Lorem Ipsum is simply dummy text of the printing
@@ -193,8 +217,8 @@ const Section1 = () => {
                 industry. Lorem Ipsum has been the industry's Lorem Ipsum is
                 simply dummy text
               </p>
-            </motion.div>
-          </motion.div>
+            </div>
+          </div>
         </div>
       </div>
     </section>
