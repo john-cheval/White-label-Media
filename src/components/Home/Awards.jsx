67dbbde47b215ad9a0ac28c9062c4524dd27a025
john-cheval <johncheval1@gmail.com>
2025-06-04 15:19:31 +0400

    heihgt is adjusted

--- src/components/Home/Awards.jsx
+++ src/components/Home/Awards.jsx
@@ -84,7 +84,7 @@ const Awards = ({ heading, awardsList }) => {
                       width={250}
                       height={100}
                       sizes="100vw"
-                      className="w-auto h-auto- object-cover- max-w-[200px]- md:max-w-full- max-h-[90px]"
+                      className="w-auto h-auto- object-cover- max-w-[200px]- md:max-w-full- h-[75px] 2xl:h-[90px]"
                       unoptimized={
                         process.env.NEXT_PUBLIC_IMAGE_UNOPTIMIZED === "true"
                       }
