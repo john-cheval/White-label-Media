389ca2586a3da12bdc32e6d0251fa437d132d59c
john-cheval <johncheval1@gmail.com>
2025-06-19 15:42:04 +0400

    added to the url

--- src/app/lib/generateMetadata.js
+++ src/app/lib/generateMetadata.js
@@ -11,18 +11,20 @@ async function generateMetadDataDetails(id, path, slug = false) {
     const description = data?.meta_description || "White Label Media";
     const image =
       data?.meta_image ||
-      "https://images.squarespace-cdn.com/content/v1/5952754eff7c50422f06ed84/1509519189436-0V1E4NFWZAAUFTO63696/favicon.ico?format=100w";
+      "https://chevaldemo.xyz/demo/white-label/wp-content/uploads/2025/06/white-Label.jpg";
 
     return {
       title,
       description,
       alternates: {
-        canonical: `https://www.whitelabelmedia.live/${path}`,
+        // canonical: `https://www.whitelabelmedia.live/${path}`,
+        canonical: `https://white-label-media.vercel.app/${path}`,
       },
       openGraph: {
         title,
         description,
-        url: `https://www.whitelabelmedia.live/${path}`,
+        // url: `https://www.whitelabelmedia.live/${path}`,
+        url: `https://white-label-media.vercel.app/${path}`,
         type: "website",
         images: [{ url: image, width: 1200, height: 630, alt: title }],
       },
@@ -43,7 +45,7 @@ async function generateMetadDataDetails(id, path, slug = false) {
         description: "White Label Media",
         images: [
           {
-            url: "https://images.squarespace-cdn.com/content/v1/5952754eff7c50422f06ed84/1509519189436-0V1E4NFWZAAUFTO63696/favicon.ico?format=100w",
+            url: "https://chevaldemo.xyz/demo/white-label/wp-content/uploads/2025/06/white-Label.jpg",
             width: 1200,
             height: 630,
             alt: "White Label Media",
@@ -53,7 +55,7 @@ async function generateMetadDataDetails(id, path, slug = false) {
       twitter: {
         card: "summary_large_image",
         images: [
-          "https://images.squarespace-cdn.com/content/v1/5952754eff7c50422f06ed84/1509519189436-0V1E4NFWZAAUFTO63696/favicon.ico?format=100w",
+          "https://chevaldemo.xyz/demo/white-label/wp-content/uploads/2025/06/white-Label.jpg",
         ],
       },
     };
